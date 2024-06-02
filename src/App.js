b4bd729a20ce7aa408f82e277b827f980832893b
mucahitcetin <mucahitcetin01@gmail.com>
2024-06-02 16:47:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import Scoops from "./components/Scoops";
+import Toppings from "./components/Toppings";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column gap-5 bg-black min-vh-100 text-light px-3 py-5">
+      <Scoops />
+
+      <Toppings />
+
+      <Form />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Form/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <form className="mb-5 d-flex jutify-content-center align-items-center gap-3">
+      <input
+        //onaylama kabul etme kutucuğu
+        onChange={(e) => setIsChecked(e.target.checked)}
+        id="terms"
+        type="checkbox"
+        className="form-check-input"
+      />
+
+      <div className="terms-wrapper">
+        <p
+          style={{
+            visibility: isHover ? "visible" : "hidden",
+          }}
+        >
+          Size gerçekten bir şey teslim etmeyeceğiz
+        </p>
+        <label htmlFor="terms">Koşulları okudum ve kabul ediyorum</label>
+      </div>
+
+      <button
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        //onaylanma gerçekleşirse buton aktif olur
+        disabled={!isChecked}
+        className="btn btn-primary"
+      >
+        Siparişi Onayla
+      </button>
+    </form>
+  );
+};
+
+export default Form;
